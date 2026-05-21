d227a94f9a4fa7a8d2ad3b273bdfbe4ef5723b7d
odon-airoldi <odon.airoldi@gmail.com>
2026-05-21 14:58:53 +0200

    feat: add dataProjects module and update projects route to return project data

---
+++ backend/data/dataProjects.js
@@ -0,0 +1,42 @@
+const dataProjects = [
+    {
+        id: 1,
+        slug: 'project-1',
+        title: 'Progetto 1',
+        description: 'Descrizione progetto 1'
+    },
+    {
+        id: 2,
+        slug: 'project-2',
+        title: 'Progetto 2',
+        description: 'Descrizione progetto 2'
+    },
+    {
+        id: 3,
+        slug: 'project-3',
+        title: 'Progetto 3',
+        description: 'Descrizione progetto 3'
+    },
+    {
+        id: 4,
+        slug: 'project-4',
+        title: 'Progetto 4',
+        description: 'Descrizione progetto 4'
+    },
+    {
+        id: 5,
+        slug: 'project-5',
+        title: 'Progetto 5',
+        description: 'Descrizione progetto 5'
+    },
+    {
+        id: 6,
+        slug: 'project-6',
+        title: 'Progetto 6',
+        description: 'Descrizione progetto 6'
+    },
+
+
+]
+
+module.exports = dataProjects
--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -4,16 +4,18 @@ const express = require('express');
 // Creo un router separato per gestire solo alcune rotte
 const router = express.Router();
 
+const dataProjects = require('../data/dataProjects')
+
 // Questa rotta restituisce tutti i progetti
 router.get('/', (req, res) => {
-    res.send('All Projects');
+    res.send(dataProjects);
 });
 
 // :id è un parametro dinamico (può essere qualsiasi valore)
 router.get('/:id', (req, res) => {
-    res.send(`Project ${req.params.id}`);
+    const id = Number(req.params.id)
+    res.send(dataProjects[id - 1]);
 });
-
 
 // Esporto il router per usarlo nel server principale
 module.exports = router;
